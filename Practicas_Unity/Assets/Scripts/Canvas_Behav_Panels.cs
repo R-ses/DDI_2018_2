c90abb38d03feb96b14d5f72d39b2d122d62098a
Ramsses <rxtechtips@gmail.com>
2018-09-12 23:52:36 -0700

    canvas p2

---
+++ Practicas_Unity/Assets/Scripts/Canvas_Behav_Panels.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class Canvas_Behav_Panels : MonoBehaviour
+{
+    private bool paused = false;
+    private bool inv = false;
+    
+
+    public GameObject pausepanel;
+    public GameObject invpanel;
+    public GameObject Gamepanel;
+    public AudioSource musicmanager;
+    private bool muted = false;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            inv = false;
+            invpanel.SetActive(inv);
+            paused = !paused;
+            pausepanel.SetActive(paused);
+            
+        }
+
+        if (Input.GetKeyUp(KeyCode.I))
+        {
+            paused = false;
+            pausepanel.SetActive(paused);
+            inv = !inv;
+            invpanel.SetActive(inv);
+        }
+
+
+        if(paused || inv)
+        {
+
+            Time.timeScale = 0;
+
+        }
+
+        if(!paused && !inv)
+        {
+
+            Time.timeScale = 1;
+
+        }
+
+
+
+    }
+
+
+    public void ContinueGame()
+    {
+
+        Debug.Log("Continue");
+        paused = false;
+        pausepanel.SetActive(paused);
+
+    }
+
+    public void ResetGame()
+    {
+
+        Debug.Log("Reset");
+        SceneManager.LoadScene(1);
+
+    }
+
+  
+
+
+}
---
+++ Practicas_Unity/Assets/Scripts/Pause_Panel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause_Panel : MonoBehaviour {
+    private bool paused;
+    private GameObject pausepanel;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void ContinueGame()
+    {
+
+        Debug.Log("Continue");
+        paused = false;
+        pausepanel.SetActive(paused);
+
+    }
+
+    public void ResetGame()
+    {
+
+        Debug.Log("Reset");
+        SceneManager.LoadScene(1);
+
+    }
+}
