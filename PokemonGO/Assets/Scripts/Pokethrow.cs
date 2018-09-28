b3a2e6eca7da747affddf95111ca5ef49fdd69cc
Ramsses <rxtechtips@gmail.com>
2018-09-27 23:36:54 -0700

    PKMNGO

---
+++ PokemonGO/Assets/Scripts/Pokethrow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pokethrow : MonoBehaviour {
+
+    public GameObject PokeballPrefab;
+	public float throwSpeed = 20f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.LookAt(Vector3.zero);
+		GameObject pokeball =  Instantiate(PokeballPrefab, transform.position, transform.rotation );
+		
+		pokeball.GetComponent<Rigidbody>().AddForce(transform.forward * throwSpeed, ForceMode.Impulse);
+		
+	}
+}
